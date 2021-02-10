e76bb0673f2222320661806c8f75f07bc877909f
Charan <57930933+charan-3876@users.noreply.github.com>
2021-02-10 16:04:42 +0530

    Update views.py

--- onlinejobportal/jobportal/views.py
+++ onlinejobportal/jobportal/views.py
@@ -5,4 +5,11 @@ def Home(request):
     return render(request,'home.html')
 
 def latest_job(request):
-    return render(request,'latest_job.html')
+    newjobs = NewJobs.objects.all()
+    data = {
+        'newjobs':newjobs
+    }
+    return render(request,'latest_job.html',data)
+
+def userlogin(request):
+    return render(request,'userlogin.html')
