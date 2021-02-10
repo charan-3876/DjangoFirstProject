2766971201cf6fccf34dc0c0e2ea2fb1a50c65aa
Charan <57930933+charan-3876@users.noreply.github.com>
2021-02-10 11:30:25 +0530

    update models.py

--- onlinejobportal/jobportal/models.py
+++ onlinejobportal/jobportal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import date
 from django.contrib.auth.models import User 
 # Create your models here.
+class NewJobs(models.Model):
+
+   SerialNumber = models.IntegerField()
+   company = models.CharField(max_length=255)
+   title = models.CharField(max_length=255)
+   location = models.CharField(max_length=255)
+   postdate = models.DateTimeField(blank=True)
+   lastdate = models.DateTimeField(blank=True)
+   action = models.CharField(max_length=255)
+
