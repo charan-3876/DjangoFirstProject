19b4ef036775a5f4fecb04a0c6459a2302095b6d
Charan <57930933+charan-3876@users.noreply.github.com>
2021-02-15 18:57:05 +0530

    Update views.py

--- onlinejobportal/jobportal/views.py
+++ onlinejobportal/jobportal/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . models import NewJobs,Rdetails,Alog
 # Create your views here.
 def Home(request):
     return render(request,'home.html')
@@ -13,3 +13,21 @@ def latest_job(request):
 
 def userlogin(request):
     return render(request,'userlogin.html')
+
+def recruiterlogin(request):
+    rdetails = Rdetails.objects.all()
+    data = {
+        'rdetails':rdetails
+    }
+    return render(request,'recruiterlogin.html',data)
+
+def adminlogin(request):
+    alog = Alog.objects.all()
+    data = {
+        'alog':alog
+    }
+    return render(request,'adminlogin.html',data)
+
+def contact(request):
+    return render(request,'contact.html')
+
