c9f09ab43c22a9aa683aae2eb66e1aaabd03736b
Charan <57930933+charan-3876@users.noreply.github.com>
2021-02-15 18:55:35 +0530

    Update models.py

--- onlinejobportal/jobportal/models.py
+++ onlinejobportal/jobportal/models.py
@@ -40,4 +40,12 @@ class NewJobs(models.Model):
 
    def __str__(self):
       return self.company
+   
+ class Alog(models.Model):
+
+   company = models.CharField(max_length=255)
+   title = models.CharField(max_length=255)
+
+   def __str__(self):
+      return self.company
 
